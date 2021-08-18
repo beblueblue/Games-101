bb7ecb6b264150bae25f18d8e5a20af9d4dbdc38
lawrence.huang <lawrence.huang@ringcentral.com>
2021-08-18 17:21:04 +0800

    finish BVH with SAH

--- demo-06/Assignment6/BVH.cpp
+++ demo-06/Assignment6/BVH.cpp
@@ -13,6 +13,8 @@ BVHAccel::BVHAccel(std::vector<Object *> p, int maxPrimsInNode,
         return;
 
     root = recursiveBuild(primitives);
+    // BVH with SAH
+    // root = SAH_recursiveBuild(primitives);
 
     time(&stop);
     double diff = difftime(stop, start);
@@ -90,6 +92,103 @@ BVHBuildNode *BVHAccel::recursiveBuild(std::vector<Object *> objects)
 
         node->left = recursiveBuild(leftshapes);
         node->right = recursiveBuild(rightshapes);
+
+        node->bounds = Union(node->left->bounds, node->right->bounds);
+    }
+
+    return node;
+}
+
+// SAH(Surface Area Heuristic)
+// http://15462.courses.cs.cmu.edu/fall2015/lecture/acceleration/slide_024
+// https://www.cnblogs.com/lookof/p/3546320.html
+BVHBuildNode *SAH_recursiveBuild(std::vector<Object *> objects)
+{
+    BVHBuildNode *node = new BVHBuildNode();
+
+    // Compute bounds of all primitives in BVH node
+    Bounds3 bounds;
+    for (int i = 0; i < objects.size(); ++i)
+        bounds = Union(bounds, objects[i]->getBounds());
+    if (objects.size() == 1)
+    {
+        // Create leaf _BVHBuildNode_
+        node->bounds = objects[0]->getBounds();
+        node->object = objects[0];
+        node->left = nullptr;
+        node->right = nullptr;
+        return node;
+    }
+    else if (objects.size() == 2)
+    {
+        node->left = SAH_recursiveBuild(std::vector{objects[0]});
+        node->right = SAH_recursiveBuild(std::vector{objects[1]});
+
+        node->bounds = Union(node->left->bounds, node->right->bounds);
+        return node;
+    }
+    else
+    {
+        Bounds3 centroidBounds;
+        for (int i = 0; i < objects.size(); ++i)
+            centroidBounds =
+                Union(centroidBounds, objects[i]->getBounds().Centroid());
+        int dim = centroidBounds.maxExtent();
+        switch (dim)
+        {
+        case 0:
+            std::sort(objects.begin(), objects.end(), [](auto f1, auto f2) {
+                return f1->getBounds().Centroid().x <
+                       f2->getBounds().Centroid().x;
+            });
+            break;
+        case 1:
+            std::sort(objects.begin(), objects.end(), [](auto f1, auto f2) {
+                return f1->getBounds().Centroid().y <
+                       f2->getBounds().Centroid().y;
+            });
+            break;
+        case 2:
+            std::sort(objects.begin(), objects.end(), [](auto f1, auto f2) {
+                return f1->getBounds().Centroid().z <
+                       f2->getBounds().Centroid().z;
+            });
+            break;
+        }
+
+        auto beginning = objects.begin();
+        auto middling = objects.begin();
+        auto ending = objects.end();
+        float cost = 0;
+        Bounds3 a;
+        Bounds3 b;
+        for (int i = 0; i < objects.size() - 1; ++i)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                a = Union(a, objects[j]->getBounds());
+            }
+            for (int j = i + 1; j < objects.size() - 1; j++)
+            {
+                b = Union(b, objects[j]->getBounds());
+            }
+            float tempcost = (i + 1) * a.SurfaceArea() / bounds.SurfaceArea() + (objects.size() - i - 1) * b.SurfaceArea() / bounds.SurfaceArea();
+            if (i == 0)
+                cost = tempcost;
+            if (tempcost < cost)
+            {
+                middling = objects.begin() + i;
+                cost = tempcost;
+            }
+        }
+
+        auto leftshapes = std::vector<Object *>(beginning, middling);
+        auto rightshapes = std::vector<Object *>(middling, ending);
+
+        assert(objects.size() == (leftshapes.size() + rightshapes.size()));
+
+        node->left = SAH_recursiveBuild(leftshapes);
+        node->right = SAH_recursiveBuild(rightshapes);
 
         node->bounds = Union(node->left->bounds, node->right->bounds);
     }
